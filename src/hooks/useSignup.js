07b38176495befae8ff706f5919ff995cbb349f8
shivanshi-s <shivanshi.saxena2@gmail.com>
2022-12-26 14:01:53 +0530

    Dispatching login action

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import { projectAuth } from '../firebase/config';
+import { useAuthContext } from './useAuthContext';
 
 export const useSignup = () => {
    // states for error and loading
    const [error, setError] = useState(null)
    const [isPending, setIsPending] = useState(false)
+   // destructor dispatch function
+   const { dispatch } = useAuthContext()
 
    // function to sign users up with the properties listed in fb
    const signup =  async (email,password, displayName) => {
@@ -15,7 +18,7 @@ export const useSignup = () => {
       try {
          // siugnup user using async await, using auth-> creates user and stores email and pwrd
         const res = await projectAuth.createUserWithEmailAndPassword(email,password)
-        console.log(res.user) //user it just created
+        //console.log(res.user) //user it just created
 
         if(!res) {
          throw new Error('Could not complete Sign up')
@@ -23,6 +26,10 @@ export const useSignup = () => {
 
       // add display name to user since auth doesnt do that for us takes in an object
       await res.user.updateProfile({displayName})
+
+      // dispatch login action
+      dispatch( { type : 'LOGIN', payload : res.user})
+
 
       setIsPending(false)
       setError(null)
