f2b6f3b7414450f7ecb5d2e5391685ad779cf269
shivanshi-s <shivanshi.saxena2@gmail.com>
2022-12-29 20:32:06 +0530

    route guarding using Redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { useAuthContext } from './hooks/useAuthContext';
 
 // pages and components
@@ -8,7 +8,7 @@ import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 
 function App() {
-   const {authIsReady} = useAuthContext()
+   const {authIsReady, user} = useAuthContext()
 
   return (
     <div className="App">
@@ -17,13 +17,16 @@ function App() {
         <Navbar/>
          <Switch>
             <Route exact path="/">
-               <Home/>
+              { !user && <Redirect to="/login" />}
+              { user && <Home/>}
             </Route>
             <Route path="/login">
-               <Login/>
+               {user && <Redirect to="/"/>}
+               {!user && <Login/>}
             </Route>
             <Route path="/signup">
-               <Signup/>
+               {user && <Redirect to="/signup"/>}
+               {!user &&<Signup/>}
             </Route>
          </Switch>
       </BrowserRouter>
